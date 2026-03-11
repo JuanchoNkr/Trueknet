fff3eb872cae77fdb81e85ae3093277a2742c78c
JuanchoNkr <1026256613@teinco.edu.co>
2026-03-10 21:11:42 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,47 @@
+function register(){
 
+let name = document.getElementById("name").value
+let email = document.getElementById("email").value
+let password = document.getElementById("password").value
+
+let users = JSON.parse(localStorage.getItem("users")) || []
+
+let user = {
+name,
+email,
+password
+}
+
+users.push(user)
+
+localStorage.setItem("users", JSON.stringify(users))
+
+alert("Usuario registrado")
+
+window.location.href="login.html"
+
+}
+
+
+function login(){
+
+let email = document.getElementById("email").value
+let password = document.getElementById("password").value
+
+let users = JSON.parse(localStorage.getItem("users")) || []
+
+let user = users.find(u => u.email === email && u.password === password)
+
+if(user){
+
+localStorage.setItem("session", JSON.stringify(user))
+
+window.location.href="dashboard.html"
+
+}else{
+
+alert("Credenciales incorrectas")
+
+}
+
+}
